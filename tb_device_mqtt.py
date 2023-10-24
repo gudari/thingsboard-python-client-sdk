84612bdcf99e6769e8e306cd7fb854a48a26e12e
gudari <aartola1986@gmail.com>
2023-10-24 16:44:46 +0200

    print tb gateway and client

--- tb_device_mqtt.py
+++ tb_device_mqtt.py
@@ -1,11 +1,11 @@
 # Copyright 2023. ThingsBoard
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,8 @@ from threading import Thread
 from simplejson import JSONDecodeError
 
 from sdk_utils import verify_checksum
+
+print("tb_client_mqtt")
 
 
 FW_TITLE_ATTR = "fw_title"
--- tb_gateway_mqtt.py
+++ tb_gateway_mqtt.py
@@ -1,11 +1,11 @@
 # Copyright 2023. ThingsBoard
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #  http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,6 +27,8 @@ GATEWAY_RPC_RESPONSE_TOPIC = "v1/gateway/rpc/response"
 GATEWAY_CLAIMING_TOPIC = "v1/gateway/claim"
 
 log = logging.getLogger("tb_connection")
+
+print("tb_gateway_mqtt")
 
 
 class TBGatewayAPI:
